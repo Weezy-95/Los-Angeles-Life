bfcd461ad3e83a29a2f065a73007b402d58bcb9d
Kai <kaibrunnert230295@gmail.com>
2023-05-16 21:20:34 +0200

    add discord auth

---
+++ Los Angeles Life/Los Angeles Life/Auth/DiscordAuth.cs
@@ -0,0 +1,45 @@
+﻿using AltV.Net;
+using Los_Angeles_Life.Entities;
+using Newtonsoft.Json;
+
+namespace Los_Angeles_Life.Auth;
+
+public class DiscordAuth : IScript
+{
+    [ClientEvent("DiscordToken")]
+    public void DiscordToken(MyPlayer player, string token)
+    {
+        using var client = new HttpClient();
+        client.DefaultRequestHeaders.Add("Authorization", $"Bearer {token}");
+
+        HttpResponseMessage response;
+        try
+        {
+            response = client.GetAsync("https://discordapp.com/api/users/@me").Result;
+        }
+        catch (AggregateException ex)
+        {
+            player.Kick("Authorization failed");
+            return;
+        }
+
+        if (!response.IsSuccessStatusCode)
+        {
+            player.Kick("Authorization failed");
+            return;
+        }
+
+        var resultString = response.Content.ReadAsStringAsync().Result;
+        dynamic result = JsonConvert.DeserializeObject(resultString) ?? throw new InvalidOperationException();
+
+        if (result == null || result.id == null || result.username == null)
+        {
+            player.Kick("Authorization failed");
+            return;
+        }
+
+        // Example of returned properties
+        Alt.Log($"Id: {result.id}");
+        Alt.Log($"Name: {result.username}#{result.discriminator}");
+    }
+}
