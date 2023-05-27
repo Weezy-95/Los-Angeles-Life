26d77a3f0d8958afb6a5d2b6d2b83723d8fb3a72
Kai <kaibrunnert230295@gmail.com>
2023-05-27 11:57:53 +0200

    Position angefangen

---
+++ Los Angeles Life/Los Angeles Life/Model/Character.cs
@@ -0,0 +1,18 @@
+﻿using AltV.Net;
+using AltV.Net.Data;
+using Los_Angeles_Life.Entities;
+
+namespace Los_Angeles_Life.Model
+{
+    public class Character : IScript
+    {
+        // Habe es nicht geschafft weiterzumachen, maybe müssen wir das doch einzeln in der DB speichern
+        public static void LastCharacterPos(MyPlayer player, Position lastPosition)
+        {
+            float playerPosX = lastPosition.X;
+            float playerPosY = lastPosition.Y;
+            float playerPosZ = lastPosition.Z;
+            
+        }
+    }
+}
