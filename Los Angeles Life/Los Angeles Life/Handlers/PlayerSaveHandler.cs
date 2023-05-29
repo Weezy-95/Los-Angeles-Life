76efceaa9d3de421431ee1f03887cf802520b87f
Kai <kaibrunnert230295@gmail.com>
2023-05-29 17:07:29 +0200

    add Doc for Interval

--- Los Angeles Life/Los Angeles Life/Handlers/PlayerSaveHandler.cs
+++ Los Angeles Life/Los Angeles Life/Handlers/PlayerSaveHandler.cs
@@ -6,6 +6,9 @@ namespace Los_Angeles_Life.Handlers
 {
     public abstract class PlayerSaveHandler : IScript
     {
+        /**
+         * Speichert alle X MS die aktuelle Position und Rotation des Spielers
+         */
         public static void SaveAllPlayersPositions(object? state)
         {
             foreach (IPlayer player in Alt.GetAllPlayers())
@@ -13,7 +16,6 @@ namespace Los_Angeles_Life.Handlers
                 Position playerPosition = player.Position;
                 Rotation playerRotation = player.Rotation;
                 DatabaseHandler.SaveAllPlayersPositions(player.DiscordId, playerPosition, playerRotation);
-                Alt.Log("Position: " + playerPosition + " Rotation: " + playerRotation);
             }
         }
     }
