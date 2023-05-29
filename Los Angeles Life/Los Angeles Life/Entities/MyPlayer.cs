cca28315b45b8e9dc4c89946d8e73bada2271fce
Kai <kaibrunnert230295@gmail.com>
2023-05-29 19:30:25 +0200

    clean up

--- Los Angeles Life/Los Angeles Life/Entities/MyPlayer.cs
+++ Los Angeles Life/Los Angeles Life/Entities/MyPlayer.cs
@@ -12,13 +12,11 @@ namespace Los_Angeles_Life.Entities
         public ulong SocialClub { get; set; }
         public long Money { get; set; }
         public int AdminLevel { get; set; }
-
         public Position PlayerPos { get; set; }
         public Rotation PlayerRot { get; set; }
         public int PlayerDim { get; set; }
         public bool IsAduty { get; set; }
         public bool IsWhitelisted { get; set; }
-        public new bool IsConnected { get; set; }
 
         public MyPlayer(ICore core, IntPtr nativePointer, ushort id) : base(core, nativePointer, id)
         {
