6637dfcc462393a5a97273f2df930d594cdeaaa1
Kai <kaibrunnert230295@gmail.com>
2023-06-05 22:44:08 +0200

    add garage_ui_backend

--- Client/src/client/systems/garage.js
+++ Client/src/client/systems/garage.js
@@ -4,6 +4,16 @@
 import alt from 'alt-client';
 import native from 'natives';
 
-alt.onServer('Client:Garage:Set', () => {
+let garage;
 
+alt.onServer('Client:Garage:Open', (name, fuel, plate) => {
+    garage = new alt.WebView("http://resource/client/webview/garage/index.html");
+    garage.emit('ShowGarage', name, fuel, plate)
+
+    setTimeout(() => {
+        if (garage) {
+            garage.destroy();
+            garage = null;
+        }
+    }, 5000);
 });
